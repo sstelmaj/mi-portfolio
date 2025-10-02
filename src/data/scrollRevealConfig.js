fc5c4c298c00a92bc870521519c184200a084ffd
sstelmaj <seba.stelmaj21@gmail.com>
2025-10-01 21:30:09 -0300

    version 1 del portfolio

--- src/data/scrollRevealConfig.js
+++ src/data/scrollRevealConfig.js
@@ -64,4 +64,11 @@ export const targetElements = [
       origin: "bottom",
     },
   },
+  {
+    element: ".project-wrapper__video",
+    animation: {
+      delay: 1000,
+      origin: window.innerWidth > 768 ? "right" : "bottom",
+    },
+  },
 ];
